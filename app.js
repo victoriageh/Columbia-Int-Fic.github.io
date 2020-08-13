d674c2c7c329b5873197c6cd31c0212a917b1208
rishic83 <68185603+rishic83@users.noreply.github.com>
2020-08-13 17:25:04 +0530

    Completed draft 1 of intro splash screen
    - The splash screen looks decent, and proceeds forward on the spacebar press.
    - One problem: the spacebar press also scrolls to the bottom of the home page with the map - how can we resolve that?

--- app.js
+++ app.js
@@ -1,8 +1,39 @@
+
+
 //Scripting for splash screen to disappear automatically
 const splash = document.querySelector('.splash');
+
+var splashy = document.getElementById('splash');
+if(splashy) {
+    splashy.addEventListener('pressSpace', checkSpacePress, false); 
+}
+
+
+//In this function, when spacebar is pressed, the alert doesn't show
+//but code proceeds to the next function. 
+function checkSpacePress(key) {
+    if(key.keyCode == "32") {
+        alert("SPACE!"); 
+    }
+}
 
 document.addEventListener('DOMContentLoaded', (e)=>{
     setTimeout(()=>{
         splash.classList.add('display-none');
-    }, 2000);
+    }, 4000); //This number indicates how long the splash overlay will stay
 })
+
+
+/* const splash = document.querySelector('.splash');
+
+var splashy = document.getElementById('splash');
+if(splashy) {
+    splashy.addEventListener('pressSpace', checkSpacePress, false); 
+}
+function checkSpacePress(key) {
+    if(key.keyCode == "32") {
+        setTimeout(()=>{
+            splash.classList.add('display-none');
+        }, 4000); //This number indicates how long the splash overlay will stay 
+    }
+} */
