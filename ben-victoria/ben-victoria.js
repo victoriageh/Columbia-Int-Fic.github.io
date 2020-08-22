00b574a9fec591b08cd28a9395cdf15cc9763418
vg2425 <vg2425@columbia.edu>
2020-08-22 18:36:39 -0400

    WECHAT UGH

--- ben-victoria/ben-victoria.js
+++
@@ -1,6 +0,0 @@
-/* click to show translate buttons and rest of first page */
-$("div.wechat-buttton").click(function(){
-        $("div.wechat-button").hide();
-        $("div.wechat-static").hide();
-        $("#1-hidden").fadeIn("slow");
-});
