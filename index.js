dd0a4b6368caa682f4bb48178af837579098568e
rishic83 <68185603+rishic83@users.noreply.github.com>
2020-08-24 16:26:21 +0530

    initial commit
    made tweaks to the twine file - first functional tech draft of the story and running on the webpage

--- index.js
+++ index.js
@@ -6,3 +6,19 @@ splash.onclick = function() {
         splash.classList.add('display-none');
     }, 0);
 }
+
+var $1 = document.getElementById('1');
+	  $1.onclick = function() {
+		  console.log("img clicker 1 clicked!");
+		  window.top.postMessage('1', '*');
+	  }
+	  var $2 = document.getElementById('2');
+	  $2.onclick = function() {
+		  console.log("img clicker 2 clicked!");
+		  window.top.postMessage('2', '*');
+	  }
+	  var $3 = document.getElementById('3');
+	  $3.onclick = function() {
+		  console.log("img clicker 3 clicked!");
+		  window.top.postMessage('3', '*');
+	  }
